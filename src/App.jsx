acaa0b53bb22a0002d3c1012518604e484ece059
Gbolahan Sholola <83152907+Sholola-Gbolahan@users.noreply.github.com>
2024-05-29 22:42:22 +0100

    Render First Person

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,20 @@
+import { useState } from "react";
+import people from "./data";
+import {FaBeer} from 'react-icons/fa'
+
 const App = () => {
-  return <h2>Reviews Starter</h2>;
+  const [index, setindex] = useState(3);
+  const {name,job,text,image }= people[index]
+
+  console.log(name)
+
+  return (
+
+    <div>
+      
+      <h2>Reviews Starter</h2>;
+      <FaBeer className="beer" />
+    </div>
+  );
 };
 export default App;
