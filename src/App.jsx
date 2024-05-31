4d92b5e1c4c50c3bea5d6f871b60dda4a2af4832
Gbolahan Sholola <83152907+Sholola-Gbolahan@users.noreply.github.com>
2024-05-31 04:19:08 +0100

    Prev and Next button functionality done

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,47 @@
 import { useState } from "react";
 import people from "./data";
-import {FaChevronCircleLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa'
+import {
+  FaChevronLeft,
+  FaChevronRight,
+  FaQuoteRight,
+} from "react-icons/fa";
 
 const App = () => {
-  const [index, setindex] = useState(0);
-  const {name,job,text,image }= people[index]
+  const [index, setIndex] = useState(0);
+  const { name, job, text, image } = people[index];
 
-  console.log(name)
+  console.log(name);
+
+  const nextPerson = () => {
+    setIndex((currentIndex)=>{
+        const newIndex = currentIndex +1
+        if( newIndex > people.length -1){
+          return 0
+        }
+        return newIndex;
+    })
+
+   
+  };
+
+  const prevPerson = () => {
+    setIndex((currentIndex)=>{
+      const newIndex = currentIndex -1
+      if( newIndex < 0){
+        return people.length -1 
+      }
+  
+      return newIndex;
+  })
+    
+  };
+
+  
 
   return (
-
     <main>
       <article className="review">
-        <div className="img-container" >
+        <div className="img-container">
           <img src={image} alt={name} className="person-img" />
           <span className="quote-icon">
             <FaQuoteRight />
@@ -21,8 +50,15 @@ const App = () => {
         <h4 className="author">{name}</h4>
         <p className="job">{job}</p>
         <p className="text">{text}</p>
-
       </article>
+      <div>
+        <button className="btn" onClick={prevPerson}>
+          <FaChevronLeft />
+        </button>
+        <button className="btn" onClick={nextPerson}>
+          <FaChevronRight />
+        </button>
+      </div>
     </main>
   );
 };
